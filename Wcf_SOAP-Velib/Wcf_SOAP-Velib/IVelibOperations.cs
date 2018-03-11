a6157621e9e9d5d9d7548737b353850e4dce8d9a
Pierre Rainero <pierre.rainero@etu.unice.fr>
2018-03-11 12:37:23 +0100

    Creation of velibOperations

---
+++ Wcf_SOAP-Velib/Wcf_SOAP-Velib/IVelibOperations.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+
+
+namespace Wcf_SOAP_Velib {
+    [ServiceContract]
+    public interface IVelibOperations {
+        [OperationContract]
+        IList<string> getStations(string city);
+
+        [OperationContract]
+        int getAvailableBikes(string city, string station);
+    }
+}
---
+++ Wcf_SOAP-Velib/Wcf_SOAP-Velib/VelibOperations.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+
+namespace Wcf_SOAP_Velib{
+    public class VelibOperations : IVelibOperations {
+        private static readonly string API_KEY = "f65cdf983437ce5da40eef104ef903fc729da72d";
+
+        public IList<string> getStations(string city){
+            JArray jsonArray = JArray.Parse(getData(city));
+            int size = jsonArray.Count;
+            IList<string> res = new List<string>();
+
+            for (int i = 0; i < size; i++)
+                res.Add((string)((JObject)jsonArray[i])["name"]);
+
+            return res;
+        }
+
+        public int getAvailableBikes(string city, string station){
+            JArray jsonArray = JArray.Parse(getData(city));
+            int size = jsonArray.Count;
+            int res = 0;
+
+            for (int i = 0; i < size; i++){
+                var item = (JObject)jsonArray[i];
+                if (station.ToLower().Contains(((string)item["name"]).ToLower()))
+                    res = (int)item["available_bikes"];
+            }
+
+            return res;
+        }
+
+        private string getData(string city) {
+            string cityName = Char.ToUpper(city[0]) + city.Substring(1, city.Length - 1).ToLower();
+            WebRequest request = WebRequest.Create("https://api.jcdecaux.com/vls/v1/stations?contract=" + cityName + "&apiKey=" + API_KEY);
+            WebResponse response = request.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            return reader.ReadToEnd();
+        }
+
+    }
+}
